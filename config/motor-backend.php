4ff7f4aa4cbb5741e0c7ecbb499c334d70b50f58
Reza Esmaili <github@dfox.info>
2016-11-22 18:12:40 +0100

    first commit

---
+++ config/motor-backend.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'models' => [
+        'client'           => Motor\Backend\Models\Client::class,
+        'language'         => Motor\Backend\Models\Language::class,
+        'user'             => Motor\Backend\Models\User::class,
+        'role'             => Motor\Backend\Models\Role::class,
+        'permission'       => Motor\Backend\Models\Permission::class,
+        'permission_group' => Motor\Backend\Models\PermissionGroup::class,
+        'email_template'   => Motor\Backend\Models\EmailTemplate::class,
+    ],
+    'routes' => [
+        'dashboard'       => true,
+        'users'           => true,
+        'roles'           => true,
+        'permissions'     => true,
+        'clients'         => true,
+        'languages'       => true,
+        'email_templates' => true,
+        'profile'         => true
+
+    ]
+];
