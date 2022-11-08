9cf2dbdc82f965acc85e241cd7db5042ffbd8646
Reza Esmaili <bitbucket@dfox.info>
2022-11-08 13:16:48 +0100

    Fix configuration bug

--- config/motor-backend.php
+++ config/motor-backend.php
@@ -2,13 +2,13 @@
 
 return [
     'models' => [
-        'client'           => Motor\Backend\Models\Client::class,
-        'language'         => Motor\Backend\Models\Language::class,
-        'user'             => Motor\Backend\Models\User::class,
-        'role'             => Motor\Backend\Models\Role::class,
-        'permission'       => Motor\Backend\Models\Permission::class,
-        'permission_group' => Motor\Backend\Models\PermissionGroup::class,
-        'email_template'   => Motor\Backend\Models\EmailTemplate::class,
+        'client'           => Motor\Admin\Models\Client::class,
+        'language'         => Motor\Admin\Models\Language::class,
+        'user'             => Motor\Admin\Models\User::class,
+        'role'             => Motor\Admin\Models\Role::class,
+        'permission'       => Motor\Admin\Models\Permission::class,
+        'permission_group' => Motor\Admin\Models\PermissionGroup::class,
+        'email_template'   => Motor\Admin\Models\EmailTemplate::class,
     ],
     'routes' => [
         'dashboard'       => true,
